6ced5f9b2b5a81136b867d8523b3f51aa65622ef
Takuchan <awatykhall850@outlook.com>
2021-02-06 23:47:45 +0900

    FirstCommit and finish

--- simplecount/ViewController.swift
+++ simplecount/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var Label: UILabel!
+    
+    var number = 0
+    @IBAction func Upbutton(_ sender: Any) {
+        number = number + 1
+        Label.text = String(number)
+    }
+    
+    @IBAction func DownButton(_ sender: Any) {
+        if number < 1{
+            return
+        }else{
+            number = number - 1
+            Label.text = String(number)
+            
+        }
+    }
+    @IBAction func ResetButton(_ sender: Any) {
+        number = 0
+        Label.text = String(number)
+    }
+    
 }
 
